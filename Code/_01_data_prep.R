0e37dc0ed79da6a3abbf61cf5f7e06663974a859
theloniousgoerz <theloniouslgoerz@gmail.com>
2026-02-21 09:57:20 -0500

    Initialize.

---
+++ Code/_01_data_prep.R
@@ -0,0 +1,26 @@
+# =============================================================================
+# Project:     1EME Submission
+# Script:      Data Prep
+# Description: This script imports necessary data and preps it for estimation.
+# =============================================================================
+# Author(s):   Thelonious Goerz
+#              Alvaro Padilla-Pozo
+# Affiliation: Cornell University
+# =============================================================================
+# Created:     02-21-21
+# Modified:    [YYYY-MM-DD]
+# Version:     1.0
+# =============================================================================
+# Input:        Data/Mortality data; Denominator data
+# Output:       [Output file(s) or objects produced]
+# =============================================================================
+# Notes:
+#   - 
+# =============================================================================
+# Install 
+# install.packages("excessmort")
+# Packages 
+library(tidyverse)
+library(magrittr)
+library(excessmort)
+# =============================================================================
